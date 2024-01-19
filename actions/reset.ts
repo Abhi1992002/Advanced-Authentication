bba73ba10c903c676be1c5a5a8f72a2073c98aee
abhi1992002 <abhimanyu1992002@gmail.com>
2024-01-19 21:05:55 +0530

    adding login , register, email verification

---
+++ actions/reset.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { getUserByEmail } from "@/data/user";
+import { resetSchema } from "@/schemas";
+import * as z from "zod";
+
+export const reset = async (values: z.infer<typeof resetSchema>) => {
+  const validatedFields = resetSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid email!" };
+  }
+
+  const { email } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+  
+  if
+};
