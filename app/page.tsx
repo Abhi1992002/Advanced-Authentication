8eca70def0f71d1d8aa7994e56eb0a051a52f5a5
abhi1992002 <abhimanyu1992002@gmail.com>
2024-02-20 21:17:09 +0530

    clean complete landing page

--- app/page.tsx
+++ app/page.tsx
@@ -11,23 +11,6 @@ const font = Poppins({
 });
 export default function Home() {
   return (
-    <main className="flex h-full flex-col justify-center items-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-      <div className="space-y-6 text-center">
-        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
-          🔐 Auth
-        </h1>
-        <p className={cn("text-white text-lg text-center", font.className)}>
-          A simple Authetication service
-        </p>
-        <div>
-          <LoginButton>
-            <Button variant={"secondary"} size={"lg"}>
-              Sign in
-            </Button>
-          </LoginButton>
-        </div>
-        <SubscribeButton />
-      </div>
-    </main>
+    <main className="flex h-full flex-col justify-center items-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800"></main>
   );
 }
